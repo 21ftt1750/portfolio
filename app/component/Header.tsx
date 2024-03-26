df53229b4af048b0407b300dafc74ff922849b47
dev-dossyr <dev.dossyr@gmail.com>
2024-03-26 11:33:29 +0800

    update Header css

--- app/component/Header.tsx
+++ app/component/Header.tsx
@@ -4,28 +4,30 @@ import Image from "next/image";
 import logo from "../../public/images/logo.png";
 
 function Header() {
-  return (
-    <div className="navbar w-full h-28 flex bg-black fixed top-0 z-10 font-roboto">
-      <div className="w-1/2 h-full">
-        <div className="w-2/6 items-center h-full flex justify-center">
-          <Link href="./">
-            <Image src={logo} alt="logo" className="w-24" />
-          </Link>
+    return (
+        <div className="w-full fixed top-0 z-10 bg-black ">
+            <div className="navbar w-[85%] h-28 flex justify-between  font-roboto mx-auto">
+                <div className="h-full">
+                    <div className="items-center h-full flex justify-center">
+                        <Link href="./">
+                            <Image src={logo} alt="logo" className="w-24" />
+                        </Link>
+                    </div>
+                </div>
+                <div className="h-full text-white flex items-center space-x-16">
+                    <Link href="/aboutme">
+                        <h4>About me</h4>
+                    </Link>
+                    <Link href="/project1">
+                        <h4>Projects</h4>
+                    </Link>
+                    <Link href="/contactme">
+                        <h4>Contact Information</h4>
+                    </Link>
+                </div>
+            </div>
         </div>
-      </div>
-      <div className="w-1/2 justify-between h-full text-white flex items-center px-28">
-        <Link href="/aboutme">
-          <h4>About me</h4>
-        </Link>
-        <Link href="/project1">
-          <h4>Projects</h4>
-        </Link>
-        <Link href="/contactme">
-          <h4>Contact Information</h4>
-        </Link>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default Header;
